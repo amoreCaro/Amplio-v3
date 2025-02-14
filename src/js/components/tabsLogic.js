bb164a28b0db6ec8dd805ce2a6e34acabc68d1e9
mac <mac@MacBook-Pro-3.local>
2025-02-14 06:58:00 +0200

    commit

--- src/js/components/tabsLogic.js
+++ src/js/components/tabsLogic.js
@@ -1,93 +1,13 @@
 export default function tabsLogic() {
-    const tabsButtons = $('.btn');
-    const tabsList = $('.tabs__list');
-  
-    // Якщо немає елементів для табів, то виводимо помилку
-    if (tabsButtons.length === 0 || tabsList.length === 0) {
-      console.error('Tabs buttons or tabs list not found');
-      return;
-    }
-  
-    // Додаємо клас "active" першому елементу і табу по дефолту
-    const firstButton = $(tabsButtons[0]);
-    const firstTabTarget = firstButton.attr('tab-target');
-    $(firstButton).addClass('active');
-    $(firstTabTarget).addClass('active');
-  
-    // Обробник кліку для кнопок табів
-    tabsButtons.on('click', function () {
-      // Видаляємо клас "active" з усіх кнопок і табів
-      tabsButtons.removeClass('active');
-      tabsList.removeClass('active');
-  
-      // Додаємо клас "active" на ту кнопку і таб, який був клікнутий
-      const tabTarget = $(this).attr('tab-target');
-      $(this).addClass('active');
-      $(tabTarget).addClass('active');
-    });
-  }
-  
+  // Set the first tab to be active by default
+  $(".btn-tabs").first().addClass("active");
+  $(".tabs__list").first().addClass("active");
 
-// export default function tabsLogic() {
-//     const tabsButtons = $('.btn-tabs');
-//     const tabsList = $('.tabs__list');
-  
-//     // Якщо немає елементів для табів, то виводимо помилку
-//     if (tabsButtons.length === 0 || tabsList.length === 0) {
-//       console.error('Tabs buttons or tabs list not found');
-//       return;
-//     }
-  
-//     // Додаємо клас "active" першому елементу і табу по дефолту
-//     $(tabsButtons[0]).addClass('active');
-//     $(tabsList[0]).addClass('active');
-  
-//     // Обробник кліку для кнопок табів
-//     tabsButtons.on('click', function () {
-//       // Видаляємо клас "active" з усіх кнопок і табів
-//       tabsButtons.removeClass('active');
-//       tabsList.removeClass('active');
-  
-//       // Додаємо клас "active" на ту кнопку і таб, який був клікнутий
-//       const tabTarget = $(this).data('tabTarget');
-//       $(this).addClass('active');
-//       $(tabTarget).addClass('active');
-//     });
-//   }
-  
+  $(".btn-tabs").on("click", function() {
+      const tabsId = $(this).data("tabTarget");
 
-
-
-
-
-
-
-// $(".btn-tabs").click(function () {
-//     let targetId = $(this).attr("data-target"); // Get the data-target attribute of the clicked button
-
-//     // Remove active class from all buttons and tabs
-//     $(".btn-tabs").removeClass("active");
-//     $(".tabs__list").removeClass("active");
-
-//     // Hide all tabs by setting display to none
-//     $(".tabs__list").css('display', 'none');
-
-//     $(this).addClass("active"); // Add active class to the clicked button
-//     $("#" + targetId).addClass("active"); // Add active class to the corresponding tab content
-
-//     // Apply the grid layout to the active tab
-//     $("#" + targetId).css({
-//         'display': 'grid',
-//         'grid-template-columns': 'repeat(3, 1fr)',
-//         'gap': '10px'
-//     });
-// });
-
-// // Initial state (activate first button and first tab)
-// $(".btn-tabs").first().addClass("active");
-// $(".tabs__list").first().addClass("active");
-// $(".tabs__list").first().css({
-//     'display': 'grid',
-//     'grid-template-columns': 'repeat(3, 1fr)',
-//     'gap': '10px'
-// });
+      // Add the active class to the clicked tab button and its corresponding content
+      $(this).addClass("active").siblings().removeClass("active");
+      $("#" + tabsId).addClass("active").siblings().removeClass("active");
+  });
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,15 @@
 // import aos from './components/aos';
-import news from './components/news';
-import sidebarLogic from './components/sidebarLogic';
-import megaMenu from './components/megaMenu';
-import modal from './components/modal';
+// import news from './components/news';
+// import sidebarLogic from './components/sidebarLogic';
+// import megaMenu from './components/megaMenu';
+// import modal from './components/modal';
 import tabsLogic from './components/tabsLogic';
 
 $( document ).ready(function() {
-
-    news();
-    sidebarLogic();
-    megaMenu();
-    modal();
     tabsLogic();
+    // news();
+    // sidebarLogic();
+    // megaMenu();
+    // modal();
     // aos(); 
 });
