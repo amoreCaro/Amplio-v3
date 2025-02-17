6cb424fc4772c66bf54560b95cd8ad4c9a1172e0
mac <mac@MacBook-Pro-3.local>
2025-02-17 21:46:05 +0200

    implemented input number using slider by inputId

--- src/js/components/calculator.js
+++ src/js/components/calculator.js
@@ -1,28 +1,30 @@
 export default function calculator() {
-    // ініціалізуємо значення за замовчуванням
-    $('.calculator__input-number').each(function() {
-        var fieldId = $(this).attr('id');
+    // Ініціалізуємо значення за замовчуванням для кожного input та слайдера
+    $('.calculator__input-number').each(function () {
+        var fieldId = $(this).attr('id'); // Отримуємо ID для input
         var value = $(this).val();
         // Додаємо текст за замовчуванням після значення
         $('#' + fieldId).val(value + ' грн.'); // Текст за замовчуванням
-        $('#' + fieldId + '_range').val(value); // синхронізуємо слайдер
+        $('#' + fieldId + '_range').val(value); // Синхронізуємо слайдер
     });
 
     // Синхронізуємо значення текстового поля та слайдера
     $('.calculator__input-number').on('input', function () {
-        var fieldId = $(this).attr('id');
-        var value = $(this).val();
-        // Додаємо текст після значення
+        var fieldId = $(this).attr('id'); // Отримуємо ID для input
+        var value = $(this).val().replace(' грн.', ''); // Отримуємо значення без "грн."
+        // Оновлюємо значення слайдера
         $('#' + fieldId + '_range').val(value);
-        $('#' + fieldId).val(value + ' грн.'); // Текст після числа
+        // Додаємо текст "грн." після значення в input
+        $('#' + fieldId).val(value + ' грн.');
     });
 
     // Синхронізуємо значення слайдера та текстового поля
     $('.calculator__slider').on('input', function () {
-        var fieldId = $(this).attr('id').replace('_range', '');
+        var fieldId = $(this).attr('id').replace('_range', ''); // Отримуємо ID для відповідного input
         var value = $(this).val();
-        // Додаємо текст після значення
-        $('#' + fieldId).val(value + ' грн.'); // Текст після числа
+        // Оновлюємо значення input з текстом "грн."
+        $('#' + fieldId).val(value + ' грн.');
+        // Синхронізуємо значення слайдера
         $('#' + fieldId + '_range').val(value);
     });
 }
