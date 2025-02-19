7f1ce6c3e0eb36fd10b13e43063a75aa4974155f
mac <mac@MacBook-Pro-3.local>
2025-02-19 13:09:37 +0200

    home fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,9 @@
-// import aos from './components/aos';
-// import news from './components/news';
-// import sidebarLogic from './components/sidebarLogic';
-// import megaMenu from './components/megaMenu';
-// import modal from './components/modal';
-import tabsLogic from './components/tabsLogic';
-import calculator from './components/calculator';
+
+import tabsLogic from './components/new/tabsLogic';
+import calculator from './components/new/calculator';
 
 $( document ).ready(function() {
     tabsLogic();
     calculator();
-    // news();
-    // sidebarLogic();
-    // megaMenu();
-    // modal();
     // aos(); 
 });
