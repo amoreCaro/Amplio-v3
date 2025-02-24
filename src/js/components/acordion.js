4d17c5ffcbe29339c899b1924ece92aa4340ef84
mac <mac@MacBook-Pro-3.local>
2025-02-24 17:12:28 +0200

    added margin between faq__item  active

--- src/js/components/acordion.js
+++ src/js/components/acordion.js
@@ -1,12 +1,30 @@
+// export default function accordion() {
+//   $(".faq__item").on("click", function() {
+//     const $this = $(this);
+//     const tabsId = $this.data("tabTarget");
+
+//     // Toggle 'active' class on the clicked FAQ item
+//     $this.toggleClass("active").siblings().removeClass("active");
+
+//     // Toggle the content visibility by adding/removing 'active' class
+//     $("#" + tabsId).toggleClass("active").siblings().removeClass("active");
+//   });
+// }
 export default function accordion() {
   $(".faq__item").on("click", function() {
     const $this = $(this);
     const tabsId = $this.data("tabTarget");
 
-    // Toggle 'active' class on the clicked FAQ item
-    $this.toggleClass("active").siblings().removeClass("active");
-
-    // Toggle the content visibility by adding/removing 'active' class
-    $("#" + tabsId).toggleClass("active").siblings().removeClass("active");
+    // Якщо поточний елемент має клас 'active', то прибираємо у всіх
+    if ($this.hasClass("active")) {
+      $this.removeClass("active");
+      $("#" + tabsId).removeClass("active");
+    } else {
+      // Якщо поточний елемент не має класу 'active', то прибираємо у всіх і додаємо до цього елемента
+      $(".faq__item, .faq__item-content").removeClass("active");
+      $this.addClass("active");
+      $("#" + tabsId).addClass("active");
+    }
   });
 }
+
