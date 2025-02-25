38eed02ea913521aa0dbf32f47a5793e89b59fff
mac <mac@MacBook-Pro-3.local>
2025-02-25 17:09:20 +0200

    added детальніше btn to credit

---
+++ src/js/components/showText.js
@@ -0,0 +1,32 @@
+export default function showText() {
+    // Перевіряємо кількість параграфів у кожному елементі
+    $('.credit__list-item-text').each(function () {
+        var paragraphs = $(this).find('p');
+        var toggleButton = $(this).find('.credit__item-btn');
+
+        // Якщо параграфів більше 2, додаємо кнопку "Детальніше"
+        if (paragraphs.length > 2) {
+            // Сховати всі параграфи, крім перших двох
+            paragraphs.slice(2).hide();
+
+            // Показуємо кнопку, якщо параграфів більше 2
+            toggleButton.show();
+
+            // Додавання обробника кліку для кнопки
+            toggleButton.on('click', function () {
+                if (toggleButton.text() === "Детальніше") {
+                    // Показуємо всі параграфи
+                    paragraphs.show();
+                    toggleButton.text("Згорнути");
+                } else {
+                    // Сховуємо додаткові параграфи
+                    paragraphs.slice(2).hide();
+                    toggleButton.text("Детальніше");
+                }
+            });
+        } else {
+            // Якщо параграфів 2 або менше, ховаємо кнопку
+            toggleButton.hide();
+        }
+    });
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
 import tabsLogic from './components/tabsLogic';
 import calculator from './components/calculator';
 import acordion from './components/acordion';
+import showText from './components/showText';
 
 $( document ).ready(function() {
     tabsLogic();
     calculator();
     acordion();
+    showText();
     // aos(); 
 });
